9712cbf48c2b2b32e8e464341ab0644f6c91d105
Latori12 <masterag012@gmail.com>
2026-01-19 11:08:47 +0800

    Add files via upload

---
+++ src/config/otherbutton.js
@@ -0,0 +1,43 @@
+// src/config/otherbutton.js
+export const otherbutton = [
+    {
+        title: "埋按钮",
+        url: "https://maibutton.yangdujun.top/",
+        color: "#77B3DE"
+    },
+    {
+        title: "梨按钮",
+        url: "https://www.shanerubian.online/",
+        color: "#2BBE38"
+    },
+    {
+        title: "虎按钮",
+        url: "https://zhaoshihu.shanerubian.online/",
+        color: "#FD9423"
+    },
+    {
+        title: "羽毛球按钮",
+        url: "https://xinggongyun.shanerubian.online/",
+        color: "#8877DE"
+    },
+    {
+        title: "虾按钮",
+        url: "https://xia.shanerubian.online/",
+        color: "#7CB774"
+    },
+    {
+        title: "龟按钮",
+        url: "https://kami.shanerubian.online/",
+        color: "#EC82BE"
+    },
+    {
+        title: "浣熊按钮",
+        url: "https://huanxiong.shanerubian.online/",
+        color: "#857975"
+    },
+    {
+        title: "猫丸子按钮",
+        url: "https://maruko.xiaofei.icu/audio",
+        color: "#4BA3FF"
+    }
+];
